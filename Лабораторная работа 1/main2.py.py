79503d89351826984d669cb2f1617afa843bbadf
Margaret3112 <lobankova.ritusya@gmail.com>
2024-10-24 21:39:25 +0300

    Add files via upload

---
+++ Лабораторная работа 1/main2.py.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+itog=len(list_players)
+half=itog//2
+print(list_players[:half])
+print(list_players[half:])
